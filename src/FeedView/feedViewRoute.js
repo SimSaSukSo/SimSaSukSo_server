85b5cc638b693230afc183aecb71e477c986b80d
enyung <45346786+eyl056@users.noreply.github.com>
2021-07-17 04:08:30 +0900

    [FIX] api url 수정

--- src/FeedView/feedViewRoute.js
+++ src/FeedView/feedViewRoute.js
@@ -3,7 +3,7 @@ module.exports = function(app){
     const jwtMiddleware = require('../../config/jwtMiddleware');
 
     // 2. 피드 정보 조회 API
-    app.get('/api/feedView/:idx', jwtMiddleware, feedView.getFeed);
+    app.get('/api/feeds/:idx', jwtMiddleware, feedView.getFeed);
 
     // jwt를 사용하기 위해 jwtMiddleware 를 체이닝 방식으로 추가하는 예제
     // app.get('/app/users/:userId', jwtMiddleware, user.getUserById);
